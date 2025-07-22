19b4e7da79a2bb170062bd381b11952869d63ad9
Süleyman Alp Katar <alpharveyspecter@gmail.com>
2025-07-22 09:33:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Portfolio() {
             <Image src="https://media.licdn.com/dms/image/v2/D4D03AQGHWE1pcS6Yyg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1673967642447?e=1758153600&v=beta&t=KeA7Dk2bv0cn0bp7XfRxLAgwF0hJgVSBTRqFVhGE5X8" alt="Profile" width={60} height={60} className="rounded-full" />
             <div>
               <h1 className="text-2xl font-mono">SÜLEYMAN ALP KATAR.</h1>
-              <p className="text-gray-400">Budding Data Analyst</p>
+              <p className="text-gray-400"> Web Specialist Assistant & Budding Data Analyst</p>
             </div>
           </div>
 
